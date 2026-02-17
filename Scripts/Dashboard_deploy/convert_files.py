1e4600211fd80e04405ffe1f029df3651337c42b
mathilderoger <mathilde.roger@ensta-paris.fr>
2026-02-17 15:03:11 +0100

    Convert EU27 csv files

--- Scripts/Dashboard_deploy/convert_files.py
+++ Scripts/Dashboard_deploy/convert_files.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import os
 
-df = pd.read_excel("PtX_demand_DK.xlsx")
-df.to_csv("PtX_demand_DK.csv", index=False)
+df = pd.read_excel("PtX_demand_EU27.xlsx")
+df.to_csv("PtX_demand_EU27.csv", index=False)
 
-df = pd.read_excel("2030_DK.xlsx")
-df.to_csv("2030_DK.csv", index=False)
+df = pd.read_excel("2030_EU27.xlsx")
+df.to_csv("2030_EU27.csv", index=False)
 
-df = pd.read_excel("2040_DK.xlsx")
-df.to_csv("2040_DK.csv", index=False)
+df = pd.read_excel("2040_EU27.xlsx")
+df.to_csv("2040_EU27.csv", index=False)
 
-df = pd.read_excel("2050_DK.xlsx")
-df.to_csv("2050_DK.csv", index=False)
+df = pd.read_excel("2050_EU27.xlsx")
+df.to_csv("2050_EU27.csv", index=False)
