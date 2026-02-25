1808d4670aeab32db3988eb0ece5a657db1645a5
mathilderoger <mathilde.roger@ensta-paris.fr>
2026-02-25 15:24:23 +0100

    Update tabs of dashboard

--- Scripts/Dashboard_dynamic/dashboard_final.py
+++ Scripts/Dashboard_dynamic/dashboard_final.py
@@ -96,54 +96,37 @@ st.plotly_chart(plot_sector_ptx_intensity(filtered_master, selected_country, sel
 # -------- EU27 Global energy demand and key numbers --------
 st.subheader(f"{selected_country} Global energy demand")
 
-# Get demand from both sectors
-transport_name = 'Transport'
-industry_name = 'Industry'
+transport_name,  industry_name = 'Transport', 'Industry'
 country_transport, country_transport_demand = get_country_demand(transport_data, selected_country, transport_name)
 country_industry, country_industry_demand = get_country_demand(industry_df, selected_country,industry_name)
 combined_demand = pd.concat([country_transport_demand, country_industry_demand], ignore_index=True)
-
-# Plot of both sectors
 fig_combined = create_country_combined_plot(country_transport_demand, transport_name, country_industry_demand, industry_name)
 
-# Key metrics
-t_2025 = country_transport_demand[country_transport_demand['Year'] == 2025]['Value'].values[0]
-t_2050 = country_transport_demand[country_transport_demand['Year'] == 2050]['Value'].values[0]
-t_change, t_growth = calculate_growth(t_2025, 2025, t_2050, 2050)
+sectors = [("Transport", country_transport, country_transport_demand, 2025, 2050),
+    ("Industry", country_industry, country_industry_demand, 2030, 2050)]
 
-i_2030 = country_industry_demand[country_industry_demand['Year'] == 2030]['Value'].values[0]
-i_2050 = country_industry_demand[country_industry_demand['Year'] == 2050]['Value'].values[0]
-i_change, i_growth = calculate_growth(i_2030, 2030, i_2050, 2050)
-
-# Most demanding categories 
-top_transport_2025 = highest_category_info(country_transport, 2025)[1]
-top_transport_2050 = highest_category_info(country_transport, 2050)[1]
-top_industry_2030 = highest_category_info(country_industry, 2030)[1]
-top_industry_2050 = highest_category_info(country_industry, 2050)[1]
-
-graph_eu27, key_num = st.columns((6, 4))
-with graph_eu27:
+graph_col, key_col = st.columns((6, 4))
+with graph_col:
     st.plotly_chart(fig_combined, use_container_width=True)
 
-# Second column: Key numbers for global demand
-with key_num:
-    st.subheader(transport_name)
-    st.metric("2050 demand", f"{t_2050:.2f} EJ", delta=f"{t_change:.1f} % vs 2025")
-    st.info(f"""
-            Average annual growth rate: {t_growth:.1f} % \\
-            Top category in 2025: **{top_transport_2025}** \\
-            Top category in 2050: **{top_transport_2050}**
-            """)
-    st.markdown('---')
-
-    st.subheader(industry_name)
-    st.metric("2050 demand", f"{i_2050:.4f} EJ", delta=f"{i_change:.1f} % vs 2030")
-    st.info(f"""
-            Average annual growth rate: {i_growth:.1f} % \\
-            Top category in 2030: **{top_industry_2030}** \\
-            Top category in 2050: **{top_industry_2050}**
-            """) 
-st.markdown('---')
+with key_col:
+    for name, raw_df, df, year_start, year_end in sectors:
+        start_val = df.loc[df['Year'] == year_start, 'Value'].values[0]
+        end_val = df.loc[df['Year'] == year_end, 'Value'].values[0]
+        change, growth = calculate_growth(start_val, year_start, end_val, year_end)
+        
+        # Top categories
+        top_start = highest_category_info(raw_df, year_start)[1]
+        top_end = highest_category_info(raw_df, year_end)[1]
+        
+        st.subheader(name)
+        st.metric(f"{year_end} demand", f"{end_val:.2f} EJ", delta=f"{change:.1f}% vs {year_start}")
+        st.info(f"""
+                Average annual growth rate: {growth:.1f}% \\
+                Top category in {year_start}: **{top_start}** \\
+                Top category in {year_end}: **{top_end}**
+                """)
+        st.markdown('---')
 
 
 # -------- Heatmaps of 2030 demand: Transport vs Industry --------
@@ -162,40 +145,34 @@ with col2:
     st.plotly_chart(fig_industry)
 
 
-# ---- Organize dashboard using TABS ----
-# Color scales for pie charts
-custom_blues = ['#08306b', '#2171b5', '#6baed6', '#c6dbef', '#deebf7', '#b3cde3', '#a6bddb', '#9ebcda', '#8c96c6']
-custom_reds = ['#67000d', '#cb181d', "#f55c2d"]
 
-tab1, tab2 = st.tabs(["Transport", "Industry"])
-with tab1:
-    st.subheader("Evolution of categories - Transport")
+# ---- Separate the dashboard into 2 tabs for Transport and Industry separated analysis ----
+tabs_info = [
+    {"name": "Transport", "data": country_transport, "full_data": transport_data,
+    "bar_plot": plot_main_transport_stack, "pie_plot": plot_transport_pie_charts, "heatmap_plot": plot_transport_heatmap,
+    "colors": ['#08306b', '#2171b5', '#6baed6', '#c6dbef', '#deebf7', '#b3cde3', '#a6bddb', '#9ebcda', '#8c96c6'],
+    "pie_years": [2025, 2050],
+    "heatmap_target": lambda: highest_category_info(country_transport, 2050)[0]
+    },
 
-    # ----- Bar plot for main categories -----
-    fig_main_transport = plot_main_transport_stack(country_transport, custom_blues)
-    st.plotly_chart(fig_main_transport)
+    {"name": "Industry", "data": country_industry, "full_data": industry_df,
+    "bar_plot": plot_main_industry_bar, "pie_plot": plot_industry_pie, "heatmap_plot": plot_industry_choropleth,
+    "colors": ['#67000d', '#cb181d', "#f55c2d"], 
+    "pie_years": [2030, 2050],
+    "heatmap_target": lambda: highest_category_info(country_transport, 2050)[0]
+    }
+]
 
-    # ----- Pie chars for categories -----
-    plot_transport_pie_charts(country_transport, 2025)
-    plot_transport_pie_charts(country_transport, 2050)
+tab_objs = st.tabs([tab["name"] for tab in tabs_info])
+for tab_obj, info in zip(tab_objs, tabs_info):
+    with tab_obj:
+        st.subheader(f"Evolution of categories - {info['name']}")
+        fig_bar = info['bar_func'](info["data"], info["colors"])
+        st.plotly_chart(fig_bar)
 
-    # ------ Heat maps for most consuming category --------
-    target_category = highest_category_info(country_transport, 2050)[0]
-    fig_cat_transport = plot_transport_heatmap(transport_data, target_category)
-    st.plotly_chart(fig_cat_transport, use_container_width = True, config= {"scrollZoom": False,"displayModeBar": False})
+        for year in info["pie_years"]:
+            info["pie_func"](info["data"], year)
 
-with tab2:
-    st.subheader("Evolution of categories - Industry")
-
-    # ----- Bar plot for main categories -----
-    fig_main_industry = plot_main_industry_bar(country_industry, custom_reds)
-    st.plotly_chart(fig_main_industry)
-
-    # ----- Pie chars for categories -----
-    plot_industry_pie(industry_df, 2030)
-    plot_industry_pie(industry_df, 2050)
-
-    # ------ Heat maps for most consuming category --------
-    target_industry_category = top_industry_2050
-    fig_cat_industry = plot_industry_choropleth(industry_df, target_industry_category)
-    st.plotly_chart(fig_cat_industry, use_container_width=True,config= {"scrollZoom": False,"displayModeBar": False})
+        target = info["heatmap_target"]()
+        fig_heatmap = info["heatmap_func"](info["full_data"], target)
+        st.plotly_chart(fig_heatmap, use_container_width=True, config={"scrollZoom": False, "displayModeBar": False})
